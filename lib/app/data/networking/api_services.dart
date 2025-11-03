d09424378700d7ebd3efe25cf2d69ab2d38998f5
rishinext <rishi@version-next.com>
2025-11-03 20:58:50 +0530

    Removed file logging from uploadPOD

--- lib/app/data/networking/api_services.dart
+++ lib/app/data/networking/api_services.dart
@@ -557,7 +557,6 @@ class ApiServices {
 
     // ✅ Add these lines for debugging
     log("FormData Fields: ${formData.fields}");
-    log("FormData Files: ${formData.files.map((f) => f.key + ' => ' + (f.value.filename ?? 'no-name')).toList()}");
 
     // ✅ Pass correct content type here
     return _api.post(
